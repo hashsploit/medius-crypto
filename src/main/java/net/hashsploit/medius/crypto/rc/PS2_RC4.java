af2901c0b4cf1380255747760bf3b033a582e3ff
hashsploit <hashsploit@protonmail.com>
2021-01-08 00:46:35 -0800

    Updated RC4 crypto algo

--- src/main/java/net/hashsploit/medius/crypto/rc/PS2_RC4.java
+++ src/main/java/net/hashsploit/medius/crypto/rc/PS2_RC4.java
@@ -16,11 +16,14 @@ public class PS2_RC4 implements ICipher {
 
 	private static final int STATE_LENGTH = 256;
 
-	private byte[] engineState;
-	private int x;
-	private int y;
-	private byte[] workingKey;
+	private final byte[] workingKey;
 	private CipherContext context;
+	
+	private class RC4State {
+		public byte[] engineState;
+		public int x;
+		public int y;
+	}
 
 	/**
 	 * Initialize crypto with a 512-bit key
@@ -30,21 +33,13 @@ public class PS2_RC4 implements ICipher {
 	 */
 	public PS2_RC4(byte[] key, CipherContext context) {
 		this.context = context;
-		setKey(key);
-	}
-
-	public void reset() {
-		setKey(workingKey);
-	}
-
-	private void setKey(byte[] keyBytes) {
-		setKey(keyBytes, null);
-	}
-
-	private void setKey(byte[] key, byte[] hash) {
 		workingKey = key;
-		x = 0;
-		y = 0;
+	}
+
+	private void setKey(RC4State state, byte[] key, byte[] hash) {
+		
+		state.x = 0;
+		state.y = 0;
 
 		int keyIndex = 0;
 		int li = 0;
@@ -52,14 +47,14 @@ public class PS2_RC4 implements ICipher {
 		int idIndex = 0;
 
 		// Initialize engine state
-		if (engineState == null) {
-			engineState = new byte[STATE_LENGTH];
+		if (state.engineState == null) {
+			state.engineState = new byte[STATE_LENGTH];
 		}
 
 		// reset the state of the engine
-		// Normally this initializes values 0,1..254,255 but UYA does this in reverse.
+		// Normally this initializes values 0,1..254,255 but this RC4 implemenation does it in reverse.
 		for (int i = 0; i < STATE_LENGTH; i++) {
-			engineState[i] = (byte) ((STATE_LENGTH - 1) - i);
+			state.engineState[i] = (byte) ((STATE_LENGTH - 1) - i);
 		}
 
 		if (hash != null && hash.length == 4) {
@@ -68,12 +63,12 @@ public class PS2_RC4 implements ICipher {
 				int v1 = hash[idIndex];
 				idIndex = (idIndex + 1) & 3;
 
-				byte temp = engineState[cipherIndex];
+				byte temp = state.engineState[cipherIndex];
 				v1 += li;
 				li = (temp + v1) & 0xFF;
 
-				engineState[cipherIndex] = engineState[li];
-				engineState[li] = temp;
+				state.engineState[cipherIndex] = state.engineState[li];
+				state.engineState[li] = temp;
 
 				cipherIndex = (cipherIndex + 5) & 0xFF;
 
@@ -93,56 +88,54 @@ public class PS2_RC4 implements ICipher {
 			keyIndex += 1;
 			keyIndex &= 0x3F;
 
-			int cipherByte = engineState[cipherIndex];
+			int cipherByte = state.engineState[cipherIndex];
 			byte cipherValue = (byte) (cipherByte & 0xFF);
 
 			cipherByte += keyByte;
 			li = cipherByte & 0xFF;
 
-			byte t0 = engineState[li];
-			engineState[cipherIndex] = t0;
-			engineState[li] = cipherValue;
+			byte t0 = state.engineState[li];
+			state.engineState[cipherIndex] = t0;
+			state.engineState[li] = cipherValue;
 
 			cipherIndex += 3;
 			cipherIndex &= 0xFF;
 		} while (cipherIndex != 0);
 	}
 
-	private byte[] _decrypt(byte[] input, int inOff, int length, int outOff) {
-		final byte[] output = new byte[length];
+	private void _decrypt(RC4State state, byte[] input, int inOff, int length, byte[] output, int outOff) {
 
 		for (int i = 0; i < length; ++i) {
-			y = (y + 5) & 0xFF;
+			state.y = (state.y + 5) & 0xFF;
 
-			int v0 = engineState[y];
+			int v0 = state.engineState[state.y];
 			byte a2 = (byte) (v0 & 0xFF);
-			v0 += x;
-			x = (int) (v0 & 0xFF);
+			v0 += state.x;
+			state.x = (int) (v0 & 0xFF);
 
-			v0 = engineState[x];
-			engineState[y] = (byte) (v0 & 0xFF);
-			engineState[x] = a2;
+			v0 = state.engineState[state.x];
+			state.engineState[state.y] = (byte) (v0 & 0xFF);
+			state.engineState[state.x] = a2;
 
-			byte a0 = input[i];
+			byte a0 = input[i + inOff];
 
 			v0 += a2;
 			v0 &= 0xFF;
-			int v1 = engineState[v0];
+			int v1 = state.engineState[v0];
 
 			a0 ^= (byte) v1;
-			output[i] = a0;
-
-			v1 = engineState[a0] + x;
-			x = v1 & 0xFF;
+			output[i + outOff] = a0;
+			
+			v1 = state.engineState[a0] + state.x;
+			state.x = v1 & 0xFF;
 		}
-
-		return output;
 	}
 
 	@Override
 	public SCERTDecryptedData decrypt(byte[] data, byte[] hash) {
 
 		byte[] plain = new byte[data.length];
+		RC4State state = new RC4State();
 
 		// Check if empty hash
 		// If hash is 0, the data is already in plaintext
@@ -152,35 +145,30 @@ public class PS2_RC4 implements ICipher {
 		}
 
 		// Set seed
-		setKey(workingKey, hash);
+		setKey(state, workingKey, hash);
 
-		plain = _decrypt(data, 0, data.length, 0);
+		_decrypt(state, data, 0, data.length, plain, 0);
+		
 		final byte[] chkHash = hash(plain);
-
 		return new SCERTDecryptedData(plain, Utils.sequenceEquals(hash, chkHash));
 	}
 
-	private byte[] _encrypt(byte[] input, int inOff, int length, int outOff) {
-		byte[] output = new byte[length];
+	private void _encrypt(RC4State state, byte[] input, int inOff, int length, byte[] output, int outOff) {
 
 		for (int i = 0; i < length; ++i) {
-			x = (x + 5) & 0xff;
-			y = (y + engineState[x]) & 0xff;
+			state.x = (state.x + 5) & 0xff;
+			state.y = (state.y + state.engineState[state.x]) & 0xff;
 
 			// Swap
-			final byte temp = engineState[x];
-			engineState[x] = engineState[y];
-			engineState[y] = temp;
+			final byte temp = state.engineState[state.x];
+			state.engineState[state.x] = state.engineState[state.y];
+			state.engineState[state.y] = temp;
 
 			// Xor
-			final byte a = input[i + inOff];
-			final byte b = engineState[(engineState[x] + engineState[y]) & 0xff];
-			output[i + outOff] = (byte) (a ^ b);
-
-			y = (engineState[input[i + inOff]] + y) & 0xff;
+			output[i + outOff] = (byte) (input[i + inOff] ^ state.engineState[(state.engineState[state.x] + state.engineState[state.y])]);
+			
+			state.y = (state.engineState[input[i + inOff]] + state.y) & 0xFF;
 		}
-
-		return output;
 	}
 
 	/**
@@ -189,12 +177,14 @@ public class PS2_RC4 implements ICipher {
 	@Override
 	public SCERTEncryptedData encrypt(byte[] data) {
 
+		RC4State state = new RC4State();
+		
 		// Set seed
 		byte[] hash = SHA1.hash(data, context);
-		setKey(workingKey, hash);
+		setKey(state, workingKey, hash);
 
 		byte[] cipher = new byte[data.length];
-		cipher = _encrypt(data, 0, data.length, 0);
+		_encrypt(state, data, 0, data.length, cipher, 0);
 
 		return new SCERTEncryptedData(cipher, hash, true);
 	}
